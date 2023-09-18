8b1523b67de8465eea761cbe79bf0eb23a59baf9
Hussam Elsayed <hussamelsayed24@gmail.com>
2023-09-18 20:50:26 +0300

    First releasr of server.py

---
+++ client.py
@@ -0,0 +1,34 @@
+import threading
+import socket
+alias = input('Enter your nickname: ')
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+ip = socket.gethostname()
+ip2 = "192.168.50.2"
+client.connect((ip, 59000))
+
+
+def client_receive():
+    while True:
+        try:
+            message = client.recv(1024).decode('utf-8')
+            if message == "alias?":
+                client.send(alias.encode('utf-8'))
+            else:
+                print(message)
+        except:
+            print('Error!')
+            client.close()
+            break
+
+
+def client_send():
+    while True:
+        message = f'{alias}: {input("")}'
+        client.send(message.encode())
+
+
+receive_thread = threading.Thread(target=client_receive)
+receive_thread.start()
+
+send_thread = threading.Thread(target=client_send)
+send_thread.start()
